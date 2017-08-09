41b9dc8a9b6750fa5c7c91023f1b7a13f2939e3e
De Wet Blomerus <dewet@gitlab.com>
2017-08-09 11:39:02 +0200

    Realtime update

--- src/App.js
+++ src/App.js
@@ -8,14 +8,61 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to De Wet's React App</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Input addNumber={this._addNumber.bind(this)} />
+        <Output startingNumber={this.state.startingNumber} />
       </div>
     );
+  }
+
+  constructor() {
+    super();
+
+    this.state = {
+      startingNumber: 0
+    }
+  }
+
+  _addNumber(number) {
+    this.setState({startingNumber: number})
   }
 }
 
 export default App;
+
+class Input extends Component {
+  render() {
+    return (
+      <form className="InputForm" onChange={this._handleSubmit.bind(this)} >
+        <label>This is my input box</label>
+        <div className="input-field">
+          <input placeholder="Starting Number:" ref={(input) => this._number = input} />
+        </div>
+        <div className="input-form-actions">
+          <button type="submit">
+            Submit Number
+          </button>
+        </div>
+      </form>
+    );
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+    
+    let number = this._number;
+
+    this.props.addNumber(number.value);
+  }
+}
+
+class Output extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>This is my output: {this.props.startingNumber}</h2>
+      </div>
+    );
+  }
+}
